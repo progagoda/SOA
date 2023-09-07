156df5fbe8c8a03954fc69d287dd7a1b35358a1e
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2023-09-07 17:04:57 +0300

    change to swagger format

--- src/main/java/com/boot/repository/ChapterRepo.java
+++
@@ -1,8 +0,0 @@
-package com.boot.repository;
-import com.boot.dto.Chapter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChapterRepo extends JpaRepository<Chapter, Long> {
-}
--- src/main/java/com/boot/repository/SpaceMarineRepo.java
+++
@@ -1,8 +0,0 @@
-package com.boot.repository;
-import com.boot.dto.SpaceMarine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpaceMarineRepo extends JpaRepository<SpaceMarine, Long> {
-}
